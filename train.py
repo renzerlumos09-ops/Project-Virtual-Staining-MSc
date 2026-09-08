5437c4bf147845c4b93f1a2836ace2608391d623
renzerlumos09-ops <renzer.lumos.09@gmail.com>
2026-09-08 18:12:04 +0800

    no message

--- train.py
+++ train.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
             epoch_iter += opt.batch_size
             model.set_input(data)  # unpack data from dataset and apply preprocessing
             model.optimize_parameters()  # calculate loss functions, get gradients, update network weights
-            torch.cuda.empty_cache()  # 清理显存缓存，防止内存泄漏
+#            torch.cuda.empty_cache()  # 清理显存缓存，防止内存泄漏
 
             if total_iters % opt.display_freq == 0:  # display images on visdom and save images to a HTML file
                 save_result = total_iters % opt.update_html_freq == 0
